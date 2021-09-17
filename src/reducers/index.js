9e200ccb7eb63bab63b5b9839c2ae9f1b0a0e411
Andy Bolos <andybolos@Andys-iMac.local>
2021-09-16 21:13:11 -0600

    lecture work-in-progress

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+
+import {dragonReducer} from "./dragonReducer";
+import {titleReducer} from "./titleReducer";
+
+export default combineReducers({
+  dragon: dragonReducer,
+  title: titleReducer
+})
