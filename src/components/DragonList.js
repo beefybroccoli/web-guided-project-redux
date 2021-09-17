bddd2614503baf914a169db71f7c8945f1297f67
Andy Bolos <andybolos@Andys-iMac.local>
2021-09-16 21:07:05 -0600

    lecture work-in-progress

--- src/components/DragonList.js
+++ src/components/DragonList.js
@@ -2,6 +2,8 @@ import React from 'react';
 import DragonMember from './DragonMember';
 
 class DragonList extends React.Component {
+
+  state= {newMember: ''}
   // state = {
   //   newMember: '',
   //   members: [
