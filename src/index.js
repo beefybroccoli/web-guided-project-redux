29244761fe02aca7f019b2be464990d1563b6321
Andy Bolos <andybolos@Andys-iMac.local>
2021-09-16 21:06:15 -0600

    lecture work-in-progress

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux';
 
 import titleReducer from './reducers/titleReducer';
+import dragonReducer from './reducers/dragonReducer'
 
 import Title from './components/Title';
 import DragonList from './components/DragonList';
