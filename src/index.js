9b8e8490405b30e9db19bdbc72b8ca0d34731014
Andy Bolos <andybolos@Andys-iMac.local>
2021-09-16 20:16:54 -0600

    lecture work-in-progress

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { createStore } from 'redux'
+
+import titleReducer from './reducers/titleReducer';
+
 import Title from './components/Title';
 import DragonList from './components/DragonList';
 import './styles.css';
+
+const store = createStore(titleReducer)
+
+console.log('store:', store);
 
 function App() {
   return (
