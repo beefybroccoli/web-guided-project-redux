71d9aade252c9b9343842781167d7cccb51da6c7
Andy Bolos <andybolos@Andys-iMac.local>
2021-09-16 20:23:05 -0600

    lecture work-in-progress

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { createStore } from 'redux'
+import { Provider } from 'react-redux';
 
 import titleReducer from './reducers/titleReducer';
 
@@ -9,16 +10,18 @@ import Title from './components/Title';
 import DragonList from './components/DragonList';
 import './styles.css';
 
-const store = createStore(titleReducer)
+const appStore = createStore(titleReducer)
 
 console.log('store:', store.getState());
 
 function App() {
   return (
+    <Provider store={appStore}
     <div className="App">
       <Title />
       <DragonList />
     </div>
+    </Provider>
   );
 }
 
